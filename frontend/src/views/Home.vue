d00fce0ebb7c314d38c0e1364fde85918eb1cbe8
Andrea Kyurchiev <andrea.kyurchiev@flaxandteal.co.uk>
2022-07-13 16:26:45 +0300

    adds pagination

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,6 +9,27 @@
           </router-link>
         </li>
       </ul>
+      <div v-if="posts && !$apollo.queries.posts.loading">
+        <div>
+          {{ posts.paginatorInfo.total }} total results
+        </div>
+        <div>
+          Page {{ posts.paginatorInfo.currentPage }} of {{ posts.paginatorInfo.lastPage }}
+        </div>
+        <div>
+          <router-link
+          v-if="posts.paginatorInfo.currentPage > 1"
+          :to="`/?page=${ posts.paginatorInfo.currentPage - 1 }`" >
+            Previous
+          </router-link>
+          &nbsp;
+          <router-link
+          v-if="posts.paginatorInfo.hasMorePages"
+          :to="`/?page=${ posts.paginatorInfo.currentPage + 1 }`" >
+            Next
+          </router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,17 +43,31 @@ export default {
   },
   apollo: {
   // Simple query that will update the 'hello' vue property
-    posts: gql`
-    query{
-      posts{
-        data{
-          id
-          title
-          body
+    posts: {
+      query: gql`
+        query getPosts($page: Int!) {
+          posts(page: $page) {
+            paginatorInfo {
+              currentPage
+              lastPage
+              total
+              hasMorePages
+            }
+            data {
+              id
+              title
+              body
+            }
+          }
         }
-      }
-    }
-    `,
+      `,
+      variables() {
+        return {
+          // eslint-disable-next-line radix
+          page: this.$route.query.page ? parseInt(this.$route.query.page) : 1,
+        };
+      },
+    },
   },
 
 };
